b8171b706e3acd4a86ce9cd0081b8c2319c0d67e
Avin19 <avinashy1993@gmail.com>
2024-12-13 21:13:20 +0530

    Game Manager

--- Assets/Project/Script/GameManager.cs
+++ Assets/Project/Script/GameManager.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace Connect.Core
+{
+    public class GameManager : MonoBehaviour
     {
-        
+        public static GameManager Instance;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                Init();
+                DontDestroyOnLoad(gameObject);
+                return;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+        private void Init()
+        {
+
+        }
+
+        #region  SCENE_LOAD
+
+        private const string MainMenu = " MainMenu";
+        private const string GamePlay = " GamePlay";
+
+
+        public void GoToMainMenu()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(MainMenu);
+        }
+
+        public void GoToGamePlay()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(GamePlay);
+        }
+
+        #endregion
     }
 }
